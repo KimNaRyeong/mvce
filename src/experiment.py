53e0de50ea27f7d85c1ac02de9a544ea968f0ebe
HyunjoonCho <jhj09016@gmail.com>
2025-05-14 17:38:39 +0900

    run with template prompts for each benchmark

--- src/experiment.py
+++ src/experiment.py
@@ -1,18 +1,21 @@
 # actual querying and storing; measure time, in-out tokens - engine.py + parser.py
 import argparse
+import json
+import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import tqdm
 
 from engine import OllamaEngine
 
 class CodeGenerator(OllamaEngine):
-    def __init__(self, endpoint, model, R=5):
+    def __init__(self, endpoint, model, R=3):
         super().__init__(endpoint, model)
         self.number_of_repetitions = R
     
-    def query_model(self):
-        prompt = "Who are you?"
+    def query_model(self, problem):
         with ThreadPoolExecutor() as executor:
-            futures = [executor.submit(self.get_LLM_response, prompt) for _ in range(self.number_of_repetitions)]
+            futures = [executor.submit(self.get_LLM_response, problem) for _ in range(self.number_of_repetitions)]
             results = []
             for future in as_completed(futures):
                 try:
@@ -21,12 +24,40 @@ class CodeGenerator(OllamaEngine):
                     print(f"Error during query: {e}")
         return results
 
+def load_benchmark(benchmark):
+    data_path = f"../data/{benchmark}.jsonl"
+    prompt_path = f"../prompt/{benchmark}_base.txt"
+    if benchmark == "HumanEval":
+        id, key = "task_id", "prompt"
+    else:
+        id, key = "id", "question"
+        
+    with open(data_path) as f:
+        data = [json.loads(line) for line in f]
+    with open(prompt_path) as f:
+        template = f.read()
+    
+    return [(sample[id], template.format(problem=sample[key])) for sample in data]
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model', default='llama3')
     parser.add_argument('-e', '--endpoint', default='http://localhost:11434/api/generate')
+    parser.add_argument('-b', '--benchmark', default='HumanEval')
+    parser.add_argument('-o', '--output_dir', default='../results')
+    parser.add_argument('-r', '--runs', default="20")
     args = parser.parse_args()
+    assert args.benchmark in ["HumanEval", "APPS"]
+    os.makedirs(args.output_dir, exist_ok=True)
     
-    generator = CodeGenerator(args.endpoint, args.model)
-    results = generator.query_model()
-    print(results)
+    benchmark = load_benchmark(args.benchmark)
+    
+    generator = CodeGenerator(args.endpoint, args.model, R=int(args.runs))
+
+    result_dict = dict()
+    for id, problem in tqdm.tqdm(benchmark):
+        result = generator.query_model(problem)
+        result_dict[id] = result
+    
+    with open(os.path.join('../results', f'{args.benchmark}.json'), 'w') as f:
+        json.dump(result_dict, f, indent=4)

60fb96f2fc4ef3201dee6a3538b37ee317782b92
HyunjoonCho <jhj09016@gmail.com>
2025-05-15 10:37:27 +0900

    zero shot generation with four models

---
+++ src/experiment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ollama pull llama3.1
+ollama pull mistral-nemo
+ollama pull qwen2.5-coder
+
+python experiment.py -m llama3 -b HumanEval -r 20
+python experiment.py -m llama3 -b APPS -r 20
+python experiment.py -m llama3.1 -b HumanEval -r 20
+python experiment.py -m llama3.1 -b APPS -r 20
+python experiment.py -m mistral-nemo -b HumanEval -r 20
+python experiment.py -m mistral-nemo -b APPS -r 20
+python experiment.py -m qwen2.5-coder -b HumanEval -r 20
+python experiment.py -m qwen2.5-coder -b APPS -r 20
